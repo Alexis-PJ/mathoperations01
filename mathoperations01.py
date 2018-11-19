655dc945fb1495c812b67a85e423ccddd715d7f1
Alexis-PJ <44810566+Alexis-PJ@users.noreply.github.com>
2018-11-19 08:32:34 -0700

    Add files via upload

---
+++ mathoperations01.py
@@ -0,0 +1,32 @@
+
+'''
+math with python
+'''
+
+a = 25
+b = 10
+c = 7
+
+# Addition
+print("display the sum of numbers ", a," + ", b," + ", c,"=",a + b + c)
+
+print("the diffrence between a, and b is :", a," - ", b," = ",a-b)
+
+print('Qoutient a/b :',a/b)
+
+# Integer division using //
+
+print("integer division of 25 with 10:",a//b)
+
+print(a//c)
+# The remainder of an integer division %
+
+print(a%c)
+
+# Multiplication
+print('product between 25 and 10:',a*b)
+print(a," * ",c," = ",a*c)
+
+# Power base **exponent
+
+print("2 to the 5 power :", 2**5)
